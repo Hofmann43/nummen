fbcecf52de70bec2e7f9ca8d3a4ad712166da964
oscarjonssson <117173831+oscarjonssson@users.noreply.github.com>
2024-02-14 16:50:43 +0100

    Nästan klar med 1a

--- Lab2/Vattentorn.m
+++ Lab2/Vattentorn.m
@@ -1,18 +1,16 @@
 clear
-g = @(x,B) (exp(B * x) + 8) ./ (1 + (x / 5).^3);
+g = @(x) (exp(0.2 * x) + 8) ./ (1 + (x / 5).^3);
 
-f = @(x) (g(x,B).^2);
-B = 0.2;
+f = @(x) (g(x).^2);
 n = 1280;
 a = 0;
 b = 20;
-T = trapets(f,n,[a b]);
-S = simpsons()
+T = trapets(f,n,a,b);
+S = simpsons(f,n,a,b);
 
 
-function T = trapets(f, n, limits)
-    a = limits(1); % Undre gränsen
-    b = limits(2); % Övre gränsen
+
+function T = trapets(f, n, a,b)
     h = (b - a) / n; % Beräknar steglängden
     x = a:h:b; % Skapar en vektor med x-värden från a till b med n intervall
     fx = f(x); % Beräknar funktionsvärdena för dessa x-värden
@@ -23,13 +21,24 @@ function T = trapets(f, n, limits)
     
 end
 
-function T1 = simpsons(f, n, limits)
-     a = limits(1); % Undre gränsen
-    b = limits(2); % Övre gränsen
+function S = simpsons(f, n, a, b)
     h = (b - a) / n; % Beräknar steglängden
-    x = a:h:b; % Skapar en vektor med x-värden från a till b med n intervall
-    fx = f(x); % Beräknar funktionsvärdena för dessa x-värden
+    odd = 0;
+    even = 0;
+    for i=1: 2: n-1
+        xi = a + (h * i);
+        odd = odd + f(xi);
+    end
+    for i=2:2:n-2
+        xi = a + (h * i);
+        even = even + f(xi);
+    end
+        S = pi*((h/3) * (f(a) + 4*odd + 2*even + f(b)));
 
-    T1 = 
 end
+
+
+
+
+
     
