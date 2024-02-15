ad69e5766f767f32620634fd677e62d845f485e9
Hofmann43 <jack@gugolz.se>
2024-02-15 14:05:15 +0100

    1a färdig

--- Lab2/Vattentorn.m
+++ Lab2/Vattentorn.m
@@ -1,44 +1,106 @@
 clear
-g = @(x) (exp(0.2 * x) + 8) ./ (1 + (x / 5).^3);
+g = @(x, B) (exp(B * x) + 8) ./ (1 + (x / 5).^3);
 
-f = @(x) (g(x).^2);
+f = @(x, B) (g(x, B).^2);
 n = 1280;
 a = 0;
 b = 20;
-T = trapets(f,n,a,b);
-S = simpsons(f,n,a,b);
+B = 0.2;
+
+T = pi*trapets(f,n,a,b, B);
+
+n_k = zeros(10,1);
+
+j = 2;
+for i=1:10
+    n_k(i,1) = j;
+    j=j*2;
+end
+
+T_k = zeros(1,length(n_k));
 
 
+for i=1:length(n_k)
+    T_k(1,i) = pi*trapets(f,n_k(i),a,b,B);
+end
 
-function T = trapets(f, n, a,b)
-    h = (b - a) / n; % Beräknar steglängden
-    x = a:h:b; % Skapar en vektor med x-värden från a till b med n intervall
-    fx = f(x); % Beräknar funktionsvärdena för dessa x-värden
-    
-    % Använd trapetsregeln för att beräkna integralen
-    T = pi*(h * (sum(fx) - 0.5 * (fx(1) + fx(end))));
-    
+test = abs(T - T_k);
+semilogy(n_k, test);
+xlabel('Number of subintervals (n)');
+ylabel('Absolute Error');
+title('Convergence Analysis');
+
+tabellT = zeros(length(n_k), 4);
+for i=1:length(n_k)
+    tabellT(i,1) = n_k(i);
+    tabellT(i,2) = T_k(i);
+    tabellT(i,3) = abs(T-T_k(i));
+    if i > 1
+        tabellT(i,4) = tabellT(i-1,3)/tabellT(i,3);
+    end
     
 end
 
-function S = simpsons(f, n, a, b)
+S = pi*simpsons(f,n,a,b, B);
+n_k = zeros(10,1);
+
+j = 2;
+for i=1:10
+    n_k(i,1) = j;
+    j=j*2;
+end
+
+T_k = zeros(1,length(n_k));
+
+for i=1:length(n_k)
+    T_k(1,i) = pi*simpsons(f,n_k(i),a,b,B);
+end
+
+figure
+testS = abs(S - T_k);
+semilogy(n_k, testS);
+xlabel('Number of subintervals (n)');
+ylabel('Absolute Error');
+title('Convergence Analysis');
+
+tabellS = zeros(length(n_k), 4);
+for i=1:length(n_k)
+    tabellS(i,1) = n_k(i);
+    tabellS(i,2) = T_k(i);
+    tabellS(i,3) = abs(S-T_k(i));
+    if i > 1
+        tabellS(i,4) = tabellS(i-1,3)/tabellS(i,3);
+    end
+    
+end
+
+
+function T = trapets(f, n, a,b, B)
+    h = (b - a) / n; % Beräknar steglängden
+    x = a:h:b; % Skapar en vektor med x-värden från a till b med n intervall
+    fx = f(x, B); % Beräknar funktionsvärdena för dessa x-värden
+    %plot(x,fx)
+    
+    % Använd trapetsregeln för att beräkna integralen
+    T = h * (sum(fx) - 0.5 * (fx(1) + fx(end)));
+    
+
+    %errT = abs(T-()
+end
+
+function S = simpsons(f, n, a, b, B)
     h = (b - a) / n; % Beräknar steglängden
     odd = 0;
     even = 0;
     for i=1: 2: n-1
         xi = a + (h * i);
-        odd = odd + f(xi);
+        odd = odd + f(xi, B);
     end
     for i=2:2:n-2
         xi = a + (h * i);
-        even = even + f(xi);
+        even = even + f(xi, B);
     end
-        S = pi*((h/3) * (f(a) + 4*odd + 2*even + f(b)));
+        S = (h/3) * (f(a, B) + 4*odd + 2*even + f(b, B));
 
 end
 
-
-
-
-
-    
